6f6dd692827d7ebf622c685758d030eb1bec1c73
steadfasterX <steadfasterx@binbash.rocks>
2025-12-19 09:50:00 +0000

    build.sh: fix temp prebuilt path

--- build.sh
+++ build.sh
@@ -95,8 +95,10 @@ build() {
 
     tagmsg "Building: $build_targets"
     ninja ${NINJA_ARGS} -C out/$1 $build_targets
+
     if [ "$?" -eq 0 ]; then
         [ "$1" '==' "x64" ] && android_arch="x86_64" || android_arch=$1
+        mkdir -p $aosmiumPath/prebuilt/$android_arch || true
         [[ "$build_targets" =~ "system_webview_apk" ]] && cp out/$1/apks/SystemWebView.apk $aosmiumPath/prebuilt/$android_arch/webview-unsigned.apk
         [[ "$build_targets" =~ "chrome_public_apk" ]] && cp out/$1/apks/ChromePublic.apk $aosmiumPath/prebuilt/$android_arch/browser-unsigned.apk
     fi
